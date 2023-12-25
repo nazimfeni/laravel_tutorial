79227310b4f17112b00edb474846c22905c7a84f
nazimfeni <mdnazimuddin77@gmail.com>
2023-12-25 17:23:19 +0600

    class-3

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('hello');
+Route::get('/datapass/{id}', function ($id) {
+
+    return view('datapass',['id'=>$id]);
 });
+
+Route::view('/', '/home');
+Route::view('/contact', '/contact');
+Route::view('/about', '/about');

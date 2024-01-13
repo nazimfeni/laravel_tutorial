2115439b105a0d9aed66f0113cc25ee4f22da721
nazim <nazim@jrfintel.com>
2024-01-13 19:47:22 +0600

    eloquent one to one relationship

--- resources/views/hello.blade.php
+++ resources/views/hello.blade.php
@@ -5,7 +5,36 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 <body>
     <h1>Hello World</h1>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12">
+                <h2 class="text-center my-3">Eloquent Relationship</h2>
+                <table class="table table-bordered">
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Password</th>
+                            <th>Phone</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($users as $data)
+                            <tr>
+                                <td>{{$data->name}}</td>
+                                <td>{{$data->password}}</td>
+                                <td>{{ $data->phone->name }}</td>
+                            </tr>
+                        @endforeach
+                        
+
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Models\User;
+use App\Models\Phone;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,5 +15,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('hello');
+    // $phone = User::find(1);
+    // $phone = User::find(1)->phone;
+    // return $phone;
+
+    // $user = Phone::find(1);
+    // $user = Phone::find(1)->user;
+    // return $user;
+
+    $users = User::all();
+    // return $users;
+    return view('hello',compact('users'));
 });
